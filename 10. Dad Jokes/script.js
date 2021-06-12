91468e4f86bdae847289d58b6b3367213b279e67
Abhishek Pratap Singh <apsingh394@gmail.com>
2021-06-12 20:40:49 +0530

    Day 10

---
+++ 10. Dad Jokes/script.js
@@ -0,0 +1,17 @@
+const joke=document.querySelector(".joke");
+const jokeBtn=document.querySelector(".jokeBtn");
+
+getJokes();
+
+jokeBtn.addEventListener("click",getJokes);
+
+async function getJokes(){
+    const config={
+        headers : {
+            "Accept" : 'application/json'
+        }
+    }
+    const res = await fetch('https://icanhazdadjoke.com/',config);
+    const data = await res.json();
+    joke.innerText=data.joke;
+}
